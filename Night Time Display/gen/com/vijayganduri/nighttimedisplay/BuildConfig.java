066833cf49b3f0d4027b64f0cf48575adfd11631
Vijay Ganduri <vijay.cellapp@exaget.com>
2013-03-16 18:22:54 +0530

    folder structure

---
+++ Night Time Display/gen/com/vijayganduri/nighttimedisplay/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vijayganduri.nighttimedisplay;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Night Time Display
+================
+
+Android App to display the current time in large digital font.
+Works in 24 hour/12 hour mode.
+Can customize the font color
